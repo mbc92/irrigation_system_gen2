76a60d3bbb14a8aab9a8152f2a1ce4511a3251fd
Chris <test@test.nl>
2025-09-27 17:39:06 +0000

    intgermediate commit after MCAL update

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,15 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include <dio.h>
+#include <dioFactory.h>
 
 
-
+using namespace MCAL;
 
 extern "C" void app_main(void)
 {
-   
+   MCAL::Dio dio;
+   uint32_t channel{0u};
+
+   dio.ReadChannel(channel);
 }
